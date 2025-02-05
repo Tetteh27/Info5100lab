57647b89eac074f823b4f11ff187c437c7878f4e
Tetteh27 <tetteh.e@northeastern.edu>
2025-02-04 21:15:20 -0500

    Lab 3 Assignment

---
+++ Infoclass/Lab3assignment/src/model/User.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author ProBook
+ */
+public class User {
+    //data members
+    private String name;
+    private String lastname;
+    private String gender = "";
+    private int age;
+    private int phonenumber;
+    private String email = "";
+    private String hobbies;
+    //method 
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(int phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(String hobbies) {
+        this.hobbies = hobbies;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "name=" + name + ", lastname=" + lastname + ", gender=" + gender + ", age=" + age + ", phonenumber=" + phonenumber + ", email=" + email + ", hobbies=" + hobbies + '}';
+    }
+    
+}
