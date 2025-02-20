336857259a64fccc2a2593faef4c37dec5728618
Tetteh27 <tetteh.e@northeastern.edu>
2025-02-19 20:18:38 -0500

    Lab 4 Assignment

---
+++ Infoclass/Lab5javaFX/src/model/User.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author ProBook
+ */
+public class User {
+    private String name;
+    private int age;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return  "Name: " + name + "\n" +
+                       
+            "Age: " + age + "\n" +
+            
+            "Email: " + email;
+            
+    }
+    
+}
